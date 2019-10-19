9ef7c160dd661c389f2c51efc401ad381479c233
Perrym14 <malikap14@gmail.com>
2019-10-19 15:10:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventAndDelegatePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
